de85f29583701fabbb0f0fb496a37ea05794dbf7
amit <amit156362@gmail.com>
2018-12-18 13:02:12 +0530

    react build deployed as static content

---
+++ src/main/resources/static/precache-manifest.d0eee223ed81f86935d78e9903d6b3cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8793d934545a31af5b7b",
+    "url": "/static/js/main.8793d934.chunk.js"
+  },
+  {
+    "revision": "3bab79d0de397100f52f",
+    "url": "/static/js/1.3bab79d0.chunk.js"
+  },
+  {
+    "revision": "8793d934545a31af5b7b",
+    "url": "/static/css/main.c2d58b7e.chunk.css"
+  },
+  {
+    "revision": "3bab79d0de397100f52f",
+    "url": "/static/css/1.f631acfd.chunk.css"
+  },
+  {
+    "revision": "5a33e4c553646dd738d49f41dc8d7890",
+    "url": "/index.html"
+  }
+];
